184189c93b4ddd97565ef3b1660cbefa7bf76ae4
hpdiddy <76453609+HPDiddy@users.noreply.github.com>
2022-01-10 18:38:25 +0000

    Update deploy_prometheus.sh

--- deploy_prometheus.sh
+++ deploy_prometheus.sh
@@ -15,7 +15,11 @@ else
 	sleep 1
 	cd prometheus && wget https://github.com/prometheus/prometheus/releases/download/v2.32.1/prometheus-2.32.1.linux-amd64.tar.gz
 	sleep 2
-	tar -xvzf prometheus-2.32.1.linux-amd64.tar.gz && mv prometheus-2.32.1.linux-amd64 promethus
+	tar -xvzf prometheus-2.32.1.linux-amd64.tar.gz && mv prometheus-2.32.1.linux-amd64/ promethus/
+	cd prometheus && mv * ..
+	#Clean up after your self timothy 
+	sudo rm prometheus-2.32.1.linux-amd64.tar.gz
+	cd .. && sudo rm -rf prometheus/
 	echo "Installing prometheus Systemd service"
 fi
 	if [ -f /etc/systemd/system/prometheus.service ]
