d47425e023aa66192acdbe19cd957ab9baf07eff
hpdiddy <76453609+HPDiddy@users.noreply.github.com>
2022-01-11 18:30:00 +0000

    Update deploy_prometheus.sh

--- deploy_prometheus.sh
+++ deploy_prometheus.sh
@@ -8,7 +8,7 @@ fi
 #Install prometheus monitoring service
 if [ -d /opt/prometheus ]
 then
-	echo "prometheus is already installed..."
+	echo "Prometheus is already installed ✅"
 else
 	echo "installing prometheus now...."
 	cd /opt && wget https://github.com/prometheus/prometheus/releases/download/v2.32.1/prometheus-2.32.1.linux-amd64.tar.gz
@@ -23,11 +23,11 @@ else
 	sudo rm prometheus-2.32.1.linux-amd64.tar.gz
 	echo "Prometheus directory was installed sucessfully"
 	sleep 2
-	echo "Installing prometheus Systemd service"
+	echo "Installing prometheus Systemd service...."
 fi
 	if [ -f /etc/systemd/system/prometheus.service ]
 	then 
-	echo "Prometheus is already installed"
+	echo "Prometheus is already installed ✅"
 	else
 echo "Installing prometheus service file....."
 		sleep 3
@@ -38,4 +38,26 @@ echo "Installing prometheus service file....."
 		echo "prometheus service has finished installing...."
 		sudo systemctl status prometheus.service
 fi
+clear
+echo "Checking Install..."
+sleep 2
+if [ -d /opt/prometheus/prometheus.yml ]
+then
+echo "Prometheus Config File ✅"
+  else
+echo "Prometheus Config File ❌"
+fi
+if [ -f /opt/prometheus/data ]
+then
+echo "Prometheus Data Folder ✅"
+  else
+echo "Prometheus Config File ❌"
+if [ -f /etc/systemd/system/prometheus.service ]
+then
+echo "Prometheus Config File ✅"
+  else
+echo "Prometheus Config File ❌"
+fi
 echo "The install is now complete and is accessible via http://*:9090"
+fi
+fi
